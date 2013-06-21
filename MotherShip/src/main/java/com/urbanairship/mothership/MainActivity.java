d3df701bed6ec57bee4de8c0f501cc54645d3e57
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2013-06-21 07:17:20 -0700

    Remove full screen ui comments.  Move urls as constants to top of class.

--- MotherShip/src/main/java/com/urbanairship/mothership/MainActivity.java
+++ MotherShip/src/main/java/com/urbanairship/mothership/MainActivity.java
@@ -1,38 +1,32 @@
 package com.urbanairship.mothership;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class MainActivity extends Activity {
-    
+
+    private static final String CALENDAR_URL = "http://calendar.google.com/";
+    private static final String TWITTER_URL = "http://twitter.com/urbanairship/";
+    private static final String LAKEHURT_LEDGER_URL = "https://urbanairship.jiveon.com/groups/lakehurst-ledger/";
+    private static final String CONTACTS_URL = "https://www.google.com/contacts/";
+    private static final String EGENCIA_URL = "https://www.egencia.com/en";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main_screen);
         
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
+
         findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://calendar.google.com/"));
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(CALENDAR_URL));
 				startActivity(browserIntent);
 				
 			}
@@ -41,7 +35,7 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://twitter.com/urbanairship/"));
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(TWITTER_URL));
 				startActivity(browserIntent);
 				
 			}
@@ -50,7 +44,7 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://urbanairship.jiveon.com/groups/lakehurst-ledger/"));
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(LAKEHURT_LEDGER_URL));
 				startActivity(browserIntent);
 				
 			}
@@ -59,7 +53,7 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/contacts/"));
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(CONTACTS_URL));
 				startActivity(browserIntent);
 				
 			}
@@ -68,7 +62,7 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.egencia.com/en"));
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(EGENCIA_URL));
 				startActivity(browserIntent);
 				
 			}
